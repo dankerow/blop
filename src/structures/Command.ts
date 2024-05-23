c1d346162c9e1eed1154e5ea4b3001e480c578c6
Dan <danker.twist@gmail.com>
2024-05-23 09:25:46 -0700

    feat(structures): add Command base class

---
+++ src/structures/Command.ts
@@ -0,0 +1,73 @@
+import type { CommandContext, CommandOptions, PartialCommandContext } from '@/types'
+import type { Blop } from '@/structures'
+import type { APIApplicationCommandOption, APIEmbed } from 'discord.js'
+
+/**
+ * Command class represents a command in the application.
+ * It contains all the necessary properties and methods to execute a command.
+ */
+export class Command {
+  /**
+   * The client instance.
+   */
+  protected client: Blop
+
+  /**
+   * The filename of the command.
+   */
+  public _filename: string
+
+  /**
+   * The name of the command.
+   */
+  public readonly name: string
+
+  /**
+   * The category of the command.
+   */
+  public category: string
+
+  /**
+   * The description of the command.
+   */
+  public description: (context: CommandContext | PartialCommandContext) => string
+
+  /**
+   * The body of the application command.
+   */
+  public applicationCommandBody: {
+    name: string
+    description: string
+    options: APIApplicationCommandOption[]
+    dm_permission: boolean
+  }
+
+  /**
+   * Constructs a new command.
+   * @param {Blop} client - The client instance.
+   * @param {CommandOptions} options - The command options.
+   */
+  constructor(client: Blop, options: CommandOptions) {
+    this.client = client
+    this._filename = options._filename
+    this.name = options.name
+    this.category = options.category || 'miscellaneous'
+    this.description = options.description
+
+    this.applicationCommandBody = {
+      name: options.name,
+      description: options.description({ client }),
+      options: options.options ?? [],
+      dm_permission: false
+    }
+  }
+
+  /**
+   * Executes the command.
+   * @param {CommandContext} context - The command context.
+   * @throws {Error} If the command doesn't have an execute method.
+   */
+  execute(context: CommandContext): string | { content?: string; embeds?: APIEmbed[]; files?: any[]; components?: any[] } | null | undefined | void | Promise<string | { content?: string; embeds?: APIEmbed[]; files?: any[]; components?: any[] } | null | undefined | void> {
+    throw new Error(`${this.name} doesn't have an execute() method.`)
+  }
+}
--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,31 @@
-import type { Snowflake } from 'discord.js'
+import type { Blop } from '@/structures'
+import type {
+  APIApplicationCommandOption,
+  ChatInputCommandInteraction,
+  Snowflake
+} from 'discord.js'
 
 export interface Config {
   dirs?: {
     commands?: string
   }
   maintainers?: Snowflake[]
+}
+
+export interface CommandOptions {
+  _filename: string
+  name: string
+  category?: string
+  description: (context: CommandContext | PartialCommandContext) => string
+  options?: APIApplicationCommandOption[]
+}
+
+export interface CommandContext {
+  client: Blop
+  interaction: ChatInputCommandInteraction<'cached'>
+}
+
+export interface PartialCommandContext {
+  client: Blop
+  interaction?: ChatInputCommandInteraction<'cached'>
 }
