777cb2f9ff4ace6b924bffc076374deab7494aa7
Dan <danker.twist@gmail.com>
2024-08-22 15:36:06 -0400

    feat(commands): add avatar command
    Displays a specific user's avatar or your avatar, with the possibility to download the avatar in different formats

---
+++ src/commands/miscellaneous/avatar.ts
@@ -0,0 +1,54 @@
+import type { CommandContext } from '@/types'
+import type { Blop } from '@/structures'
+
+import { Command } from '@/structures'
+import { ApplicationCommandOptionType } from 'discord.js'
+
+export default class Avatar extends Command {
+  constructor(client: Blop) {
+    super(client, {
+      _filename: import.meta.url,
+      name: 'avatar',
+      description: () => 'Displays a specific user\'s avatar or your avatar.',
+      options: [
+        {
+          name: 'user',
+          description: 'An user to get the avatar from',
+          type: ApplicationCommandOptionType.User
+        }
+      ]
+    })
+  }
+
+  execute({ interaction }: CommandContext) {
+    let { user } = interaction
+
+    const userArg = interaction.options.getUser('user')
+    if (userArg) {
+      user = userArg
+
+      if (interaction.guild.members.cache.has(userArg.id)) {
+        user = interaction.guild.members.cache.get(userArg.id)!.user
+      }
+    }
+
+    const formats = ['webp', 'png', 'jpg', 'jpeg']
+
+    if (user.avatar && user.avatar.startsWith('a_')) formats.push('gif')
+
+    return {
+      embeds: [
+        {
+          image: { url: user.displayAvatarURL({ extension: 'png', size: 512 }) },
+          fields: [
+            {
+              name: 'Avatar',
+              value: `${user.username}\n\`Download links:\` ${formats.map((format) => `[${format.toUpperCase()}](${user.displayAvatarURL({ extension: format, size: 512 })})`).join(' | ')}`
+            }
+          ],
+          color: 7154431
+        }
+      ]
+    }
+  }
+}
