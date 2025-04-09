3da42c1dc78768d446f43056ef028b1053f1464d
Dan <danker.twist@gmail.com>
2025-04-09 15:46:01 -0700

    feat(commands): add language command for setting preferred guild language

---
+++ src/commands/administration/language.ts
@@ -0,0 +1,64 @@
+import type { CommandContext } from '@/types'
+import type { Blop } from '@/structures'
+
+import { Command } from '@/structures'
+import { ApplicationCommandOptionType } from 'discord.js'
+
+export default class Language extends Command {
+  constructor(client: Blop) {
+    super(client, {
+      _filename: import.meta.url,
+      name: 'language',
+      options: [
+        {
+          name: 'language',
+          description: 'The language to set',
+          type: ApplicationCommandOptionType.String,
+          choices: client.config.i18n.languages.map((lang) => ({
+            name: lang.localeName,
+            value: lang.aliases[0]
+          }))
+        }
+      ]
+    })
+  }
+
+  async execute({ client, interaction, data }: CommandContext) {
+    const languageArg = interaction.options.getString('language', false)
+
+    if (!languageArg) {
+      const currentLanguage = client.config.i18n.languages.find((lang) => lang.iso === data.guild.language)?.localeName
+
+      return interaction.translate('language.current-language', {
+        language: currentLanguage
+      })
+    }
+
+    const language = client.config.i18n.languages.find((lang) => lang.aliases.includes(languageArg.toLowerCase()))
+
+    if (!language) {
+      const availableLanguages = client.config.i18n.languages.map((lang) => lang.localeName).join('`, `')
+
+      return `${interaction.translate('language.unknown-language')} ${interaction.translate('language.available-languages', { languages: availableLanguages })}`
+    }
+
+    if (language.iso === data.guild.language) {
+      return interaction.translate('language.already-set', {
+        language: language.localeName
+      })
+    }
+
+    await client.database.guild.update({
+      where: {
+        id: data.guild.id
+      },
+      data: {
+        language: language.iso
+      }
+    })
+
+    return interaction.translate('language.language-changed', {
+      language: language.localeName
+    })
+  }
+}
