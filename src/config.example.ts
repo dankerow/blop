e3c200c0475e4d53bec0f4fa259e914a84debf63
Dan <danker.twist@gmail.com>
2025-04-11 12:31:07 -0700

    feat: add internationalization example configuration

--- src/config.example.ts
+++ src/config.example.ts
@@ -2,6 +2,31 @@ import type { Config } from '@/types'
 
 export default {
   maintainers: [],
+  i18n: {
+    options: {
+      debug: process.env.NODE_ENV === 'development',
+      fallbackLng: 'en-US'
+    },
+    languages: [
+      {
+        iso: 'en-US',
+        localeName: 'English',
+        aliases: [
+          'en',
+          'english'
+        ]
+      },
+      {
+        iso: 'fr',
+        localeName: 'Français',
+        aliases: [
+          'fr',
+          'français',
+          'french'
+        ]
+      }
+    ]
+  },
   apis: {
     rawgo: {
       baseUrl: ''
